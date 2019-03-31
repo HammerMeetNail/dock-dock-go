eb1cc4b7d7f49b0da1a61a3cf0e3b4be1d28a76e
Dave O'Connor <1656866+HammerMeetNail@users.noreply.github.com>
2019-03-31 11:49:07 -0400

    Add docker example, update gitignore remove vendor

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-
+	stats.GetImages()
 }
--- pkg/stats/system.go
+++ pkg/stats/system.go
@@ -1,1 +1,26 @@
 package stats
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+// GetImages lists all images currently on host
+func GetImages() {
+	cli, err := client.NewClientWithOpts(client.WithVersion("1.39"))
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+	}
+}
